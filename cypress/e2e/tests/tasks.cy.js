245a4f9dd3b843207155b041fecba11d0dc07e37
Thais Paes <thaisreginapaesdossantos@gmail.com>
2025-11-04 15:05:50 -0300

    Feat: aprendendo requests

--- cypress/e2e/tests/tasks.cy.js
+++ cypress/e2e/tests/tasks.cy.js
@@ -1,11 +1,19 @@
-import {faker} from '@faker-js/faker'
 
 describe("tarefas", () => {
   it("deve cadastrar uma nova tarefa", () => {
+
+    cy.request ({
+        url: 'http://localhost:3000/helper/tasks',
+        method: 'DELETE',
+        body: {name: 'Estudar Cypress'}
+    }).then(response => {
+        expect(response.status).to.eq(204);
+    });
+
     cy.visit("http://localhost:3000/");
 
     cy.get('input[placeholder="Add a new Task"]')
-      .type(faker.music.songName());
+      .type('Estudar Cypress');
 
 
     cy.contains('button', 'Create').click()
